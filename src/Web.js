0d9a4a711d0f7490df1a56219632fba5a0e3ee65
Kalpesh Mahale <67682474+Kalpesh1012@users.noreply.github.com>
2023-01-20 15:34:15 +0530

    Add files via upload

---
+++ src/Web.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Aboutus from "./components/Aboutus";
+import ErrorHandling from "./components/ErrorHandling";
+import Contact from "./components/Contact";
+import RestaurantDetails from "./components/RestaurantDetails";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Outlet />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+const approuter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorHandling />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <Aboutus />,
+        errorElement: <ErrorHandling />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+        errorElement: <ErrorHandling />,
+      },
+      {
+        path: "/restuarant/:uniqueid",
+        element: <RestaurantDetails />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={approuter} />);
---
+++ src/components/Aboutus.js
@@ -0,0 +1,13 @@
+const Aboutus = () => {
+  return (
+    <>
+      <h1>Welcome to the our App khao Piyo and sojao</h1>
+      <p>
+        We started our app on 26 december 2022 and we offer some amazing food
+        for you. This company is started by Kalpesh mahale with the guidance of
+        Akshay Saini
+      </p>
+    </>
+  );
+};
+export default Aboutus;
